bbad1ec9e3550bf67219bf473a49bf771161367a
Vikas <xanderbilla@gmail.com>
2024-12-30 06:17:01 +0530

    Error1# Transaction Fixed

--- src/main/java/com/xander/demo/DemoApplication.java
+++ src/main/java/com/xander/demo/DemoApplication.java
@@ -2,23 +2,22 @@ package com.xander.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 @SpringBootApplication
-@EnableTransactionManagement
+// @EnableTransactionManagement // This annotation is moved to /config/TransactionConfig.java
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Bean
-	public PlatformTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
-		return new MongoTransactionManager(dbFactory);
-	}
+	/*
+	 * This snippet is moved to /config/TransactionConfig.java
+	 *
+	 *@Bean
+	 *public PlatformTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+	 *	return new MongoTransactionManager(dbFactory);
+	 *}
+	 * 
+	 */
 
 }
